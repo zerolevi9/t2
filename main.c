10a71067a9223de5d51ccfd078ac86114bf3764b
Levi Monteiro Silva <85647747+zerolevi9@users.noreply.github.com>
2023-11-28 14:40:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,60 +1,113 @@
+#include "fila.h"
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-#include "fila.h"
+#include <unistd.h>
 
-int main(){
-  Lista *lista_controle = inicia_Lista(); //Lista não ordenada
-  Fila *fila_SE = inicia_Fila(); //Fila sala de espera já ordenada
-  Fila *fila_raiox = inicia_Fila();//Fila de quem vai fazer ou está fazendo raio-X já ordenado
-  Fila *fila_laudo = inicia_Fila();//Fila de quem vai fazer ou está fazendo raio-X já ordenado
-  Hospital *hospital = inicia_Hospital();//Váriavel que controla os equipamentos e funcionários do hospital
+int main() {
   srand(time(NULL));
-  
-  for (int Unidade_de_tempo = 1; Unidade_de_tempo < 43200; Unidade_de_tempo++) {
-      // Verifica se um novo paciente chega com base na probabilidade
-      if (probabilidade_paciente()) {
-          // Gera um novo paciente e o adiciona à lista de controle e à fila ordenada
-          Paciente *paciente = gerarPaciente(Unidade_de_tempo);
-          insere_Lista(lista_controle, paciente);
-          insere_ordenado(fila_SE, paciente);
-      }
+  int relatorio = 11; 
+  int nextID = 1000;
+  int max_time = 43200;
+  char filename[10];
+  patient *patient;
+  int time_report_med;
+  float avgTime;
+  int cont_Normal = 0;
+  int cont_NormalTemp = 0;
+  int calc_normal = 0;
+  int cont_Pneumonia = 0;
+  int cont_PneumoniaTemp = 0;
+  int calc_Pneumonia = 0;
+  int cont_Bronquite = 0;
+  int cont_BronquiteTemp = 0;
+  int calc_Bronquite = 0;
+  int cont_Fratura = 0;
+  int cont_FraturaTemp = 0;
+  int calc_Fratura = 0;
+  int cont_Apendicite = 0;
+  int cont_ApendiciteTemp = 0;
+  int calc_Apendicite = 0;
 
-      // Atualiza o tempo para os serviços de raio-X e laudo
-      atualiza_tempo_raiox(hospital);
-      atualiza_tempo_laudo(hospital);
+  printf("Digite o nome do arquivo: ");
+  scanf("%s", filename);
 
-      // Atualiza o status dos serviços de raio-X e laudo, bem como as filas
-      atualiza_raiox(hospital, fila_raiox, Unidade_de_tempo);
-      atualiza_laudo(hospital, fila_laudo, Unidade_de_tempo);
+  ListPatient *list_patient = ListPatient_create();
+  QueueExams *exams = QueueExams_create();
+  QueueReport *report = QueueReport_create();
+  ListMachines *Machine = ListMachines_create();
+  ListRadiologist *Radio = Radiologist_create();
 
-      // Verifica se há vagas disponíveis na fila de raio-X
-      if (verifica_raiox(hospital)) {
-          // Move pacientes da fila ordenada de espera para a fila de raio-X
-          fila_atendimento_raiox(fila_SE, fila_raiox, hospital, Unidade_de_tempo);
-      }
+  FILE *arquivo = fopen(filename, "r");
 
-      // Verifica se o raio-X foi concluído e a sala de consulta está disponível
-      if (verifica_laudo(hospital)) {
-          // Move pacientes da fila de raio-X para a fila de laudo
-          fila_atendimento_laudo(fila_raiox, fila_laudo, hospital, Unidade_de_tempo);
-      }
+  if (arquivo == NULL) {
+    printf("Erro ao abrir arquivo.\n\n");
+    return 1;
+  }
+  initializeMachines(5, Machine);
+  initializeRadiologist(3, Radio);
+  for(int time=1; time <= max_time; time++) {
+    if (rand() % 5 + 1 == 1) {
+      char name[50];
+      int age = 0;
+      char cpf[20];
+      fscanf(arquivo, "%49s\n%15s\n%d\n", name, cpf, &age);
+        patient = newPatient(name, cpf, age, nextID);
+        ListPatient_insert(list_patient, patient);
+        QueueEnqueue(exams, nextID);
+        nextID++;
+    }
+    insert_machines(Machine, exams, time);
+    Exam_Record(report, Machine, time);
+    insert_radio(Radio, report, time); 
+    remove_radio(Radio,time);
 
-      // Atualiza a lista de controle com entradas e saídas de pacientes
-      atualiza_lista_controle(fila_laudo, lista_controle);
+    avgTime = averageReportTime(report);
+    cont_Normal = total_path(report, "Saúde Normal");
+    cont_NormalTemp = tempWait_path(report, "Saúde Normal");
+    calc_normal = (cont_Normal != 0) ? (cont_NormalTemp / cont_Normal) : 0;
+      
+    cont_Bronquite = total_path(report, "Bronquite");
+    cont_BronquiteTemp = tempWait_path(report, "Bronquite");
+    calc_Bronquite = (cont_Bronquite != 0) ? (cont_BronquiteTemp / cont_Bronquite) : 0;
 
-      // Imprime métricas a cada 10 unidades de tempo
-      if ((Unidade_de_tempo % 10) == 0) {
-          printa_metrica(lista_controle, hospital);
-      }
+    cont_Pneumonia = total_path(report, "Pneumonia");
+    cont_PneumoniaTemp = tempWait_path(report, "Pneumonia");
+    calc_Pneumonia = (cont_Pneumonia != 0) ? (cont_PneumoniaTemp / cont_Pneumonia) : 0;
+
+    cont_Fratura = total_path(report, "Fratura de Fêmur");
+    cont_FraturaTemp = tempWait_path(report, "Fratura de Fêmur");
+    calc_Fratura = (cont_Fratura != 0) ? (cont_FraturaTemp / cont_Fratura) : 0;
+
+    cont_Apendicite = total_path(report, "Apendicite");
+    cont_ApendiciteTemp = tempWait_path(report, "Apendicite");
+    calc_Apendicite = (cont_Apendicite != 0) ? (cont_ApendiciteTemp / cont_Apendicite) : 0;
+
+    time_report_med = examsBeyondTimeLimit(report, 7200);
+    
+    // Metricas saidas
+    if((relatorio % 10) == 0){
+      printf("\nRelatório de Métricas - Unidade de Tempo: %d\n", time);
+      printf("Tempo médio de laudo: %.0f\n", avgTime);
+      printf("Tempo médio de laudo por patologia (Saúde Normal): %d\n", calc_normal);
+      printf("Tempo médio de laudo por patologia (Bronquite): %d\n", calc_Bronquite);
+      printf("Tempo médio de laudo por patologia (Pneumonia): %d\n", calc_Pneumonia);
+      printf("Tempo médio de laudo por patologia (Fratura de Fêmur): %d\n", calc_Fratura);
+      printf("Tempo médio de laudo por patologia (Apendicite): %d\n", calc_Apendicite);
+      printf("Quantidade de exames realizados após o limite de tempo estabelecido: %d\n\n", time_report_med);
+    }
+    sleep(1);
+    relatorio = relatorio + 1;
   }
 
-// Liberando memória
-liberaLista(lista_controle);  // Libera a memória da lista de controle
-liberaFila(fila_raiox);       // Libera a memória da fila de raio-X
-liberaFila(fila_laudo);        // Libera a memória da fila de laudo
-liberaFila(fila_SE);           // Libera a memória da fila de sala de espera
-liberaHospital(hospital);      // Libera a memória da estrutura Hospital
+  listpatient_free(list_patient);
+  listmach_free(Machine);
+  listradiologist_free(Radio);
+  qexam_free(exams);
+  qreport_free(report);
 
-return 0; // Indica que o programa foi executado com sucesso
+  fclose(arquivo);
+  return 0;
 }
